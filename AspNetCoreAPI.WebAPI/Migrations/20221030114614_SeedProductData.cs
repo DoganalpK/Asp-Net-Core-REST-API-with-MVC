db39823fb534cd2ae1f94d6b2ab4f89685bc63c6
dogan <dogan@DOGANALP>
2022-10-30 14:47:16 +0300

    Add project files.

---
+++ AspNetCoreAPI.WebAPI/Migrations/20221030114614_SeedProductData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreAPI.WebAPI.Migrations
+{
+    public partial class SeedProductData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CreatedDate", "ImagePath", "Name", "Price", "Stock" },
+                values: new object[] { 1, new DateTime(2022, 10, 25, 14, 46, 14, 503, DateTimeKind.Local).AddTicks(1330), null, "Computer", 15000m, 150 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CreatedDate", "ImagePath", "Name", "Price", "Stock" },
+                values: new object[] { 2, new DateTime(2022, 10, 10, 14, 46, 14, 504, DateTimeKind.Local).AddTicks(360), null, "Phone", 10000m, 100 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CreatedDate", "ImagePath", "Name", "Price", "Stock" },
+                values: new object[] { 3, new DateTime(2022, 9, 10, 14, 46, 14, 504, DateTimeKind.Local).AddTicks(378), null, "Keyboard", 1000m, 500 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
